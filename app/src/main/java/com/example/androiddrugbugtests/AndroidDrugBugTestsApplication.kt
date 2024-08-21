20150347feacd0f1aabe234a4815aad18f7c9a85
Steven <stevefaliszewski@gmail.com>
2024-08-21 15:27:40 -0700

    Add Application class, add annotation to MainActivity, and update AndroidManifest

---
+++ app/src/main/java/com/example/androiddrugbugtests/AndroidDrugBugTestsApplication.kt
@@ -0,0 +1,8 @@
+package com.example.androiddrugbugtests
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class AndroidDrugBugTestsApplication : Application()
