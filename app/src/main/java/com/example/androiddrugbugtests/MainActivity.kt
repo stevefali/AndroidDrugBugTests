d6e34afc14e5c7e87beac7b499e6dd57da31c157
Steven <stevefaliszewski@gmail.com>
2024-09-02 00:09:06 -0700

    Add search bar for interactor search

--- app/src/main/java/com/example/androiddrugbugtests/MainActivity.kt
+++ app/src/main/java/com/example/androiddrugbugtests/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,9 +14,13 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,10 +44,20 @@ class MainActivity : ComponentActivity() {
                         .padding(horizontal = 8.dp)
                 ) { innerPadding ->
                     val interactions by viewModel.interactionsResponse.collectAsState()
+                    var interactor by remember {
+                        mutableStateOf("")
+                    }
                     Column(modifier = Modifier.padding(innerPadding)) {
-                        Button(onClick = { viewModel.getInteractions("grapefruit") }) {
-                            Text(text = "Get Interactions")
+                        Row {
+                            TextField(value = interactor, onValueChange = { interactor = it })
+                            Button(
+                                onClick = { viewModel.getInteractions(interactor) },
+                                enabled = interactor.isNotBlank()
+                            ) {
+                                Text(text = "Get Interactions")
+                            }
                         }
+
                         if (viewModel.isResultLoading.value) {
                             Text(text = "Loading...")
                         }
